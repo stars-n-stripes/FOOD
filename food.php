baf9b91ae576d1ec9f6447017faa8fffb3c39c0b
Chuck <jctlynch@gmail.com>
2019-02-18 13:31:55 -0800

    Added basic html for index and login

--- food.php
+++ food.php
@@ -5,3 +5,9 @@
  * Date: 2/17/2019
  * Time: 7:06 PM
  */
+
+$dbconn = pg_connect("host=localhost port=5432 dbname=mary user=postgres");
+
+if ($_POST['both'])
+    $query = 'SELECT count()'
+$query = 'SELECT'
---
+++ login.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jlynch
+ * Date: 2/17/2019
+ * Time: 9:21 PM
+ */
